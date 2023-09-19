d4a2102f6658c91a581f6e4d6a806163562221cf
BeyzaCevikPXL <beyza.cevik@student.pxl.be>
2023-09-19 22:11:58 +0200

    Gallerij met Fancybox

---
+++ index.js
@@ -0,0 +1,5 @@
+//click event handler: je event handler koppelen aan een gebeurtenis.
+//fancybox.bind koppelt de Fancybox-functionaliteit aan HTML-elementen met het attribuut data-fancybox;
+//{} het geeft geen specifieke aanpassingen of configuratie voor Fancybox op dit moment. bv. {effecten, loops, ...}
+
+Fancybox.bind("[data-fancybox]", {});
